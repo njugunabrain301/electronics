ce2ef9e68209d4d4d05c54dad29afb8eeb749863
njugunabrain301 <48784973+njugunabrain301@users.noreply.github.com>
2023-08-09 10:14:22 +0300

    debug under construction

--- src/Components/Loading/UnderConstruction.jsx
+++ src/Components/Loading/UnderConstruction.jsx
@@ -5,22 +5,24 @@ function UnderConstruction() {
   return (
     <div className="underconstruction">
       <h1 className="title">The Website is Currently Under Construction</h1>
-      <div className="table-container">
-        <div className="table">
-          <div className="extended-table"></div>
+      <div className="my-container">
+        <div className="table-container">
+          <div className="table">
+            <div className="extended-table"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="block-container">
-        <div className="block diagonal-block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-      </div>
+        <div className="block-container">
+          <div className="block diagonal-block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+        </div>
 
-      <div className="hammer-container">
-        <div className="hammer">
-          <div className="hammer-head"></div>
+        <div className="hammer-container">
+          <div className="hammer">
+            <div className="hammer-head"></div>
+          </div>
         </div>
       </div>
     </div>
