fd9b1e907f7f9930c4bcccd38c574051cc7a9f3d
njugunabrain301 <48784973+njugunabrain301@users.noreply.github.com>
2023-08-19 17:04:26 +0300

    added backup server

--- src/features/axios.config.js
+++ src/features/axios.config.js
@@ -6,6 +6,8 @@ const baseURL =
   production === "true"
     ? "https://bunika.cyclic.app/user/zidika"
     : "http://localhost:3001/user/zidika";
+
+const backupUrl1 = "https://bunika-api.onrender.com/user/zidika";
 let instance = axios.create({
   baseURL,
 });
@@ -22,6 +24,37 @@ instance.interceptors.request.use(
 
   (error) => {
     return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  async (config) => {
+    return config;
+  },
+
+  async (error) => {
+    let config = error.config;
+    if (!config || config.baseURL === backupUrl1) {
+      return Promise.reject(error);
+    }
+    if (config.response) {
+      return config;
+    }
+
+    config.baseURL = backupUrl1;
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, config.retryDelay || 1000);
+    });
+
+    try {
+      const retryResponse = await instance(config);
+      return retryResponse;
+    } catch (retryError) {
+      return Promise.reject(retryError);
+    }
   }
 );
 
