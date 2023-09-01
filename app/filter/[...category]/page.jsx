79f437dbdb6e6a934cf7efafb54d9de9cf6913a3
njugunabrain301 <48784973+njugunabrain301@users.noreply.github.com>
2023-09-01 19:00:54 +0300

    start debug filtered products

--- app/filter/[...category]/page.jsx
+++ app/filter/[...category]/page.jsx
@@ -12,96 +12,97 @@ import React from "react";
 export const runtime = "edge";
 
 export async function generateMetadata({ params }) {
-  let profile = await fetchBusinessProfile();
-  profile = profile.data;
-  let category = params.category[0];
-  let searchParam = "";
-  let singleProduct = false;
-  if (category === "search") {
-    searchParam = params.category[1];
-    if (params.category.length > 2) {
-      singleProduct = params.category[2];
-    }
-  } else if (params.category.length > 1) {
-    singleProduct = params.category[1];
-  }
-  let product = {};
-  if (singleProduct) {
-    let res = await fetchProduct({ pid: singleProduct });
-    product = res.data;
-    return {
-      title: product.name + " | " + profile.name,
-      description: product.description,
-    };
-  } else {
-    if (category === "search") category = searchParam;
-    return {
-      title: category + " | " + profile.name,
-      description: category + ", " + profile.name + ", " + profile.about,
-    };
-  }
+  // let profile = await fetchBusinessProfile();
+  // profile = profile.data;
+  // let category = params.category[0];
+  // let searchParam = "";
+  // let singleProduct = false;
+  // if (category === "search") {
+  //   searchParam = params.category[1];
+  //   if (params.category.length > 2) {
+  //     singleProduct = params.category[2];
+  //   }
+  // } else if (params.category.length > 1) {
+  //   singleProduct = params.category[1];
+  // }
+  // let product = {};
+  // if (singleProduct) {
+  //   let res = await fetchProduct({ pid: singleProduct });
+  //   product = res.data;
+  //   return {
+  //     title: product.name + " | " + profile.name,
+  //     description: product.description,
+  //   };
+  // } else {
+  //   if (category === "search") category = searchParam;
+  //   return {
+  //     title: category + " | " + profile.name,
+  //     description: category + ", " + profile.name + ", " + profile.about,
+  //   };
+  // }
 }
 
 export default async function Page({ params }) {
-  let category = decodeURI(params.category[0]);
-  let searchParam = "";
-  let singleProduct = false;
-  if (category === "search") {
-    searchParam = decodeURI(params.category[1]);
-    if (params.category.length > 2) {
-      singleProduct = decodeURI(params.category[2]);
-    }
-  } else if (params.category.length > 1) {
-    singleProduct = decodeURI(params.category[1]);
-  }
+  // let category = decodeURI(params.category[0]);
+  // let searchParam = "";
+  // let singleProduct = false;
+  // if (category === "search") {
+  //   searchParam = decodeURI(params.category[1]);
+  //   if (params.category.length > 2) {
+  //     singleProduct = decodeURI(params.category[2]);
+  //   }
+  // } else if (params.category.length > 1) {
+  //   singleProduct = decodeURI(params.category[1]);
+  // }
 
-  let products = [];
-  let type = category;
-  let wearables = [];
-  let profile = {};
-  let product = {};
+  // let products = [];
+  // let type = category;
+  // let wearables = [];
+  // let profile = {};
+  // let product = {};
 
-  if (singleProduct) {
-    let res = await fetchProduct({ pid: singleProduct });
-    product = res.data;
-  } else {
-    let res = await fetchProducts();
-    products = res.data;
-    if (category === "search") {
-      products = products.filter(
-        (prod) =>
-          prod.type.toLowerCase().includes(searchParam.toLowerCase()) ||
-          prod.name.toLowerCase().includes(searchParam.toLowerCase())
-      );
-    } else {
-      products = products.filter(
-        (prod) => prod.type.toLowerCase() === category.toLowerCase()
-      );
-    }
-    res = await fetchWearables();
-    wearables = res.data;
-  }
-  let res = await fetchBusinessProfile();
-  profile = res.data;
+  // if (singleProduct) {
+  //   let res = await fetchProduct({ pid: singleProduct });
+  //   product = res.data;
+  // } else {
+  //   let res = await fetchProducts();
+  //   products = res.data;
+  //   if (category === "search") {
+  //     products = products.filter(
+  //       (prod) =>
+  //         prod.type.toLowerCase().includes(searchParam.toLowerCase()) ||
+  //         prod.name.toLowerCase().includes(searchParam.toLowerCase())
+  //     );
+  //   } else {
+  //     products = products.filter(
+  //       (prod) => prod.type.toLowerCase() === category.toLowerCase()
+  //     );
+  //   }
+  //   res = await fetchWearables();
+  //   wearables = res.data;
+  // }
+  // let res = await fetchBusinessProfile();
+  // profile = res.data;
 
-  if (singleProduct && product === null) {
-    return (
-      <NotFound
-        message={"The product you are looking for could not be found"}
-      />
-    );
-  } else if (products.length === 0) {
-    if (category === "search") category = "Search:" + searchParam;
-    <NotFound
-      message={
-        "There are no products matching your search term '" + category + ""
-      }
-    />;
-  }
+  // if (singleProduct && product === null) {
+  //   return (
+  //     <NotFound
+  //       message={"The product you are looking for could not be found"}
+  //     />
+  //   );
+  // } else if (products.length === 0) {
+  //   if (category === "search") category = "Search:" + searchParam;
+  //   <NotFound
+  //     message={
+  //       "There are no products matching your search term '" + category + ""
+  //     }
+  //   />;
+  // }
 
   return (
     <div>
-      {singleProduct ? (
+      Here
+      {/* {singleProduct ? (
         <>
           <SingleProduct
             product={product}
@@ -119,7 +120,7 @@ export default async function Page({ params }) {
             searchParam={searchParam}
           ></FilteredProducts>
         </>
-      )}
+      )} */}
     </div>
   );
 }
