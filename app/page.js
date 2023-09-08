2d235cfe5555cf5913ed2b6a503576bbe58deb1e
njugunabrain301 <48784973+njugunabrain301@users.noreply.github.com>
2023-09-08 20:02:36 +0300

    added edge to index page

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ export const revalidate = 0;
 export const fetchCache = "force-no-store";
 export const dynamic = "force-dynamic";
 
+export const runtime = "edge";
+
 export default async function Home() {
   let isReady = false;
   let promoted = [];
