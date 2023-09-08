f9687cd29c473c7e79bc974a6c9ac85b41aaca53
njugunabrain301 <48784973+njugunabrain301@users.noreply.github.com>
2023-09-08 19:16:13 +0300

    tried debug again

--- utils/backendAPIs/fetch.config.js
+++ utils/backendAPIs/fetch.config.js
@@ -16,7 +16,7 @@ export const fetchData = async (url) => {
         "Content-Type": "application/json",
         business: bid,
       },
-      cache: "no-cache",
+      cache: "no-store",
     });
 
     if (!response.ok) {
